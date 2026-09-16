8d41a9a3d46a6db9d4e1e0dcce333aa999cba34e
Ernie Miller <ernie@ernie.io>
2026-09-16 19:19:13 -0400

    Return a cancelled update download to the available state

--- code/qcommon/autoupdate.c
+++ code/qcommon/autoupdate.c
@@ -1084,20 +1084,18 @@ Console command: \updatecancel
 */
 void Update_Cancel_f( void )
 {
-	if ( updateState == UPDATE_DOWNLOADING ) {
-		if ( updateZipFile ) {
-			fclose( updateZipFile );
-			updateZipFile = NULL;
-		}
-		Update_CurlCleanup();
-		remove( updateZipPath );
-		Com_Printf( "Update: download cancelled\n" );
-	} else if ( updateState == UPDATE_CHECKING ) {
-		Update_CurlCleanup();
-		Com_Printf( "Update: check cancelled\n" );
-	}
+	if ( updateState != UPDATE_DOWNLOADING )
+		return;
 
-	Update_SetState( UPDATE_IDLE );
+	if ( updateZipFile ) {
+		fclose( updateZipFile );
+		updateZipFile = NULL;
+	}
+	Update_CurlCleanup();
+	remove( updateZipPath );
+	Com_Printf( "Update: download cancelled\n" );
+
+	Update_SetState( UPDATE_AVAILABLE );
 }
 
 
